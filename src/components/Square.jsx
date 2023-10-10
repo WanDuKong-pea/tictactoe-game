4a2925434d392d48fe48ad5c5f6626c21799c837
KimYeJin <sophisticateo_o@naver.com>
2023-10-10 14:28:04 +0900

    사용자와 상호작용하는 컴포넌트 만들기-onClick 이벤트

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../index.css'
 
 const Square = () => {
+    const [value, setValue] = useState('');
+
     return (
-        <button className="square">
+
+        //ONCLICK EVENT로 STATE가 변경되면 RERENDERING 발생
+        //화면에 표시되는 값을 변경시킴
+        <button className="square" onClick={()=> {setValue('X')}}>
+            {value}
         </button>
     );
 }
